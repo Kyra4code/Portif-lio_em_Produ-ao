86c87aa5eb95a258693f8e9d08474cf5a8c93449
unknown <lucarioecharmito@gmail.com>
2024-05-20 20:43:56 -0300

    whille

---
+++ JS/Script.js
@@ -0,0 +1,22 @@
+
+// const aviso = [];
+// var teste = 0;
+// var x = 0;
+
+// while(teste < 4){
+    
+//     const carro = prompt("Digite seu carro");
+    
+//     aviso.push(carro);
+
+//     teste++;
+    
+//     x += teste;
+    
+// }
+// alert(aviso);
+
+for(let i = 0; i < 4; i++){
+    const escolha = prompt(`digite o seu ${i+1}° carro?`);
+}
+
